f9fccc94760b8603e837fff93e1a6369e724c9d0
Shreya Pramanik <100830699+ShreyaPramanik47@users.noreply.github.com>
2023-01-12 12:04:46 +0530

    Update Check Palindrome.cpp

--- Step 1.4 - Know Basic Maths/Check Palindrome.cpp
+++ Step 1.4 - Know Basic Maths/Check Palindrome.cpp
@@ -1,13 +1,4 @@
 // GFG
-
-//{ Driver Code Starts
-//Initial Template for C++
-#include<bits/stdc++.h>
-using namespace std;
-
-// } Driver Code Ends
-//User function Template for C++
-
 class Solution
 {
 	public:
@@ -30,25 +21,6 @@ class Solution
 		    return "Yes";
 		}
 };
-
-//{ Driver Code Starts.
-int main(){
-    int T;
-    cin >> T;
-    while(T--)
-    {
-    	int n;
-    	cin >> n;
-    	Solution ob;
-    	string ans = ob.is_palindrome(n);
-    	cout << ans <<"\n";
-    }
-	return 0;
-}
-
-// } Driver Code Ends
-
-
 
 
 // LEETCODE
