9a4f56b055c63a961710a114c3761fdb77d6d435
Shreya Pramanik <100830699+ShreyaPramanik47@users.noreply.github.com>
2023-01-12 12:03:47 +0530

    Update Check Palindrome.cpp

--- Step 1.4 - Know Basic Maths/Check Palindrome.cpp
+++ Step 1.4 - Know Basic Maths/Check Palindrome.cpp
@@ -1,7 +1,7 @@
+// GFG
+
 //{ Driver Code Starts
 //Initial Template for C++
-
-
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -47,3 +47,28 @@ int main(){
 }
 
 // } Driver Code Ends
+
+
+
+
+// LEETCODE
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0){
+            return 0;
+        }
+      long long ans=0;
+      int num=x;
+      while(num!=0){
+          int digit = num%10;
+           ans = (ans*10)+digit;
+          num = num/10;
+      }
+      if(ans==x){
+          return 1;
+      }
+      return 0;
+    }
+};
+
