b62801bcb6efae9c2d64bbcaafc210967a9fbbe3
Shreya Pramanik <100830699+ShreyaPramanik47@users.noreply.github.com>
2023-02-04 21:30:59 +0530

    Add files via upload

---
+++ Step 1.5 - Learn basic Recursion/Valid Palindrome.cpp
@@ -0,0 +1,21 @@
+// Leetcode
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        
+        string t="";
+        
+        for(char x:s){
+            if(isalpha(x) || isdigit(x))
+            t+=tolower(x);
+        }
+        
+        string g=t;
+        reverse(g.begin(),g.end());
+        
+        if(g!=t){
+            return false;
+        }
+        return true;
+    }
+};
