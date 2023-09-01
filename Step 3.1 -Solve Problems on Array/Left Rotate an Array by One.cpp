96432eb94895eab7e5d33bd64edeba8a9c65d090
Shreya Pramanik <100830699+ShreyaPramanik47@users.noreply.github.com>
2023-09-01 23:37:16 +0530

    Update Left Rotate an Array by One.cpp

--- Step 3.1 -Solve Problems on Array/Left Rotate an Array by One.cpp
+++ Step 3.1 -Solve Problems on Array/Left Rotate an Array by One.cpp
@@ -10,3 +10,15 @@ vector<int> rotateArray(vector<int>& arr, int n) {
 }
 
 // T.C - O(N);
+
+
+#include <bits/stdc++.h> 
+vector<int> rotateArray(vector<int>& arr, int n) {
+    // Write your code here.
+    int temp=arr[0];
+    for(int i=1; i<n; i++){
+        arr[i-1]=arr[i];
+    }
+    arr[n-1]=temp;
+    return arr;
+}
