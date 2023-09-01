c7534338b0a704d211383a4cd24cf088cc746314
Shreya Pramanik <100830699+ShreyaPramanik47@users.noreply.github.com>
2023-09-01 23:38:35 +0530

    Update Move Zero's to End.cpp

--- Step 3.1 -Solve Problems on Array/Move Zero's to End.cpp
+++ Step 3.1 -Solve Problems on Array/Move Zero's to End.cpp
@@ -1,33 +1,12 @@
 vector<int> moveZeros(int n, vector<int> a) {
-
-    int j=-1;
-
-    for(int i=0;i<n;i++) {
-
-        if(a[i]==0) {
-
-            j=i;
-
-            break;
-
-        }
-
-    }
-
-    if(j==-1) return a;
-
-    for(int i=j+1;i<n;i++) {
-
-        if(a[i]!=0) {
-
-            swap(a[i],a[j]);
-
+    // Write your code here.
+    int j=0;
+    for(int i=0; i<n; i++){
+        if(a[i]!=0){
+            swap(a[i], a[j]);
             j++;
-
         }
-
     }
-
     return a;
-
 }
+
