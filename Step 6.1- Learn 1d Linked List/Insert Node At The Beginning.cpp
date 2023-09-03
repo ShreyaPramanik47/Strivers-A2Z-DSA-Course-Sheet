6e0a8c7bd5ec0e93103d1b4266d0b201f569ed6a
Shreya Pramanik <100830699+ShreyaPramanik47@users.noreply.github.com>
2023-09-03 23:58:22 +0530

    Create Insert Node At The Beginning.cpp

---
+++ Step 6.1- Learn 1d Linked List/Insert Node At The Beginning.cpp
@@ -0,0 +1,20 @@
+/**
+ * Definition of linked list
+ * class Node {
+ *
+ * public:
+ *     int data;
+ *     Node* next;
+ *     Node() : data(0), next(nullptr) {}
+ *     Node(int x) : data(x), next(nullptr) {}
+ *     Node(int x, Node* next) : data(x), next(next) {}
+ * };
+ */
+
+Node* insertAtFirst(Node* list, int newValue) {
+    // Write your code here
+    Node* temp= new Node(newValue);
+    temp->next = list;
+    list=temp;
+    return list;
+}
